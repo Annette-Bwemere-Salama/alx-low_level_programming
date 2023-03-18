1631dab3b85b96af73f48ac4eaf21b2cdeda9666
Annette_Bwemere <bwemereannette@gmail.com>
2023-03-17 20:27:39 -0700

    refactor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ long longtype;
 long long int longinttype;
 float floattype;
 
-printf("Size of a char: %i byte(s)\n", sizeof(chartype));
-print("Size of an int: %i bytes(s)\n", sizeof(intype));
-printf("Size of a long int: %i byte(s)\n", sizeof(longtype));
-printf("Size of a long long int: %i byte(s)\n", sizeof(longinttype));
-print("Size of a float: %i byte(s)\n", sizeof(floattype));
+printf("Size of a chart: %li byte(s)\n", sizeof(chartype));
+printf("Size of an int: %li byte(s)\n", sizeof(intype));
+printf("Size of a long int: %li byte(s)\n", sizeof(longtype));
+printf("Size of a long long int: %li byte(s)\n", sizeof(longinttype));
+printf("Size of a float: %li byte(s)\n", sizeof(floattype));
 
 return (0);
 }
