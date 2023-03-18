fcb3796ab77214131d4634d210843169a874e028
Annette_Bwemere <bwemereannette@gmail.com>
2023-03-17 17:58:48 -0700

    My First Programming in c i like it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-# include <stdio.h>
-	/**
-	 *main -Print Hello Word
-	 *Return :Always 0
-	 */
-int main(void){
+#include <stdio.h>
+/**
+*main - entry point
+*Return: Always 0
+*/
+
+int main(void)
+
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-       }	
+	return (0);
+}	
