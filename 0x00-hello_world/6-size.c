a29c7206a1eb3a6f08a8c462b9caa981bf366c4a
Annette_Bwemere <bwemereannette@gmail.com>
2023-03-17 18:44:16 -0700

    size c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int intype;
+char chartype;
+long longtype;
+long long int longinttype;
+float floattype;
+
+printf("Size of a char: %i byte(s)\n", sizeof(chartype));
+print("Size of an int: %i bytes(s)\n", sizeof(intype));
+printf("Size of a long int: %i byte(s)\n", sizeof(longtype));
+printf("Size of a long long int: %i byte(s)\n", sizeof(longinttype));
+print("Size of a float: %i byte(s)\n", sizeof(floattype));
+
+return (0);
+}
