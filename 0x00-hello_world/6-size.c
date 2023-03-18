47c2f80f0aa5e9a6c9a75e0a64c7aec4827fb706
Annette_Bwemere <bwemereannette@gmail.com>
2023-03-18 03:13:49 -0700

    betty style refactor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,16 @@
 
 int main(void)
 {
-int intype;
-char chartype;
-long longtype;
-long long int longinttype;
-float floattype;
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
 
-printf("Size of a chart: %lu byte(s)\n",(unsigned long)sizeof(chartype));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(intype));
-printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(longtype));
-printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(longinttype));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(floattype));
-
-return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
 }
