a3519f64c7435720d3ff0acd4f029d657d1e3bdd
Annette Bwemere <bwemereannette@gmail.com>
2023-03-31 12:55:03 +0200

    void jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - Jack Bauer
+ * Return. Nothing
+ */
+void jack_bauer(void)
+{  
+	int i, j, k;
+
+		for (i = 0; i < 24; i++)
+		{	
+			for(j = 0; j < 6; j ++)
+			{ 	
+				for(k = 0; k < 10; k++)
+				{	
+					if (i < 10)
+						printf("0%d:%d%d\n", i, j, k);
+					else
+						printf("%d:%d%d\n", i , j , k);
+				
+				}
+			}
+		
+		}
+}
