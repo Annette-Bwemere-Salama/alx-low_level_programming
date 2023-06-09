ef7e64f4cbb9183a0a81337185da2d98368e528d
Annette Bwemere <bwemereannette@gmail.com>
2023-06-09 17:35:05 +0200

    comment

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *Program that print the name of this followed by a new line
+ *Do not remove the pqth before the name of the program
+ *
+ * */
+/*
+ *main: will return 0;
+ *it's will have (int argc, char argv[]);
+ * */
