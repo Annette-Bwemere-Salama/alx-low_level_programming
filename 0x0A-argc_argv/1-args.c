74ac68fca58c83f8708d2d16f80765b6472c8455
Annette Bwemere <bwemereannette@gmail.com>
2023-06-10 20:03:09 +0200

    short desciption

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,10 @@
 #include "main.h"
 
 /**
- * main : Program that prints the name of this followed by a new line
- * Do not remove the path before the name of the program
- *
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success
+ * main - Entry point
+ * @argv: The array of arguments
+ * @argc: The length of argv
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
