08a83b755d9d4d029c4844c94e7a3d9b0d274b7d
Annette Bwemere <bwemereannette@gmail.com>
2023-06-10 20:00:43 +0200

    add comment

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - Program that prints the name of this followed by a new line
+ * main : Program that prints the name of this followed by a new line
  * Do not remove the path before the name of the program
  *
  * @argc: argument count
