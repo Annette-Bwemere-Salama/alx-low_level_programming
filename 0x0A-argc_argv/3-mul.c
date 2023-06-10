58a7da40cf2d42b2bc720fdb8721f4a680b5a56f
Annette Bwemere <bwemereannette@gmail.com>
2023-06-10 15:49:54 +0200

    bety style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,9 +4,11 @@
 
 /*
 * white the programm that multipy two numbers
-*Your program should print the result of the multiplication, followed by a new line
-*You can assume that the two numbers and result of the multiplication can be stored in an integer
-*If the program does not receive two arguments, your program should print Error, 
+*Your program should print the result of the multiplication,
+*followed by a new line
+*You can assume that the two numbers and result of the multiplication
+*can be stored in an integer
+*If the program does not receive two arguments, your program should print Error,
 *followed by a new line, and return 1
 *
 * @argc: argument
@@ -15,11 +17,12 @@
 
 int main(int argc, char *argv[])
 {
-    int num1,num2,product;
-    if(argc != 3){
-        printf("Error\n");
-        return (1);
-    }
+    int num1, num2, product;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 
     num1 = atoi(argv[1]);
     num2 = atoi(argv[2]);
